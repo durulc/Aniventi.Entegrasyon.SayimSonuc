57c9d317a7372a459329bd4efae64865ec75a6be
durulc <82138425+durulc@users.noreply.github.com>
2022-11-03 15:38:45 +0300

    deded

---
+++ WepApi.Entegrasyon.Sayim/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WepApi.Entegrasyon.Sayim
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ WepApi.Entegrasyon.Sayim/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WepApi.Entegrasyon.Sayim.WebApiApplication" Language="C#" %>
