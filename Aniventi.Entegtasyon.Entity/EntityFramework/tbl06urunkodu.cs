1ae7bd437bc2ab98da75b9ade2f4cdc6711d5914
durulc <82138425+durulc@users.noreply.github.com>
2022-11-03 19:15:48 +0300

    dede

---
+++ Aniventi.Entegtasyon.Entity/EntityFramework/tbl06urunkodu.cs
@@ -0,0 +1,32 @@
+﻿using Aniventi.Entegtasyon.Entity.Important;
+using DevExpress.Xpo;
+
+namespace Aniventi.Entegtasyon.Entity.EntityFramework
+{
+    [Persistent("tbl06urunkodu")]
+    public class tbl06urunkodu : TabloObject
+    {
+        public tbl06urunkodu(Session session) : base(session) { }
+
+        string _12urunadi = "";
+        [Persistent("zurunadi")]
+        [Size(1000)]
+        public string zurunadi
+        {
+            get { return _12urunadi; }
+            set { SetPropertyValue<string>("zurunadi", ref _12urunadi, value); }
+        }
+
+        string _zetiketturu = "";
+        [Persistent("zetiketturu")]
+        [Size(1000)]
+        public string zetiketturu
+        {
+            get { return _zetiketturu; }
+            set { SetPropertyValue<string>("zetiketturu", ref _zetiketturu, value); }
+        }
+
+
+        //zetiketturu
+    }
+}
---
+++ Aniventi.Entegtasyon.Entity/EntityFramework/tbl07veri.cs
@@ -0,0 +1,56 @@
+﻿using Aniventi.Entegtasyon.Entity.Important;
+using DevExpress.Xpo;
+
+namespace Aniventi.Entegtasyon.Entity.EntityFramework
+{
+    [Persistent("tbl07veri")]
+    public class tbl07veri : TabloObject
+    {
+        public tbl07veri(Session session) : base(session) { }
+
+        string _odakarekod = "";
+        [Persistent("odakarekod")]
+        [Size(1000)]
+        public string odakarekod
+        {
+            get { return _odakarekod; }
+            set { SetPropertyValue<string>("odakarekod", ref _odakarekod, value); }
+        }
+
+        string _urunid = "";
+        [Persistent("urunid")]
+        [Size(1000)]
+        public string urunid
+        {
+            get { return _urunid; }
+            set { SetPropertyValue<string>("urunid", ref _urunid, value); }
+        }
+
+        string _pasifetiket = "";
+        [Persistent("pasifetiket")]
+        [Size(1000)]
+        public string pasifetiket
+        {
+            get { return _pasifetiket; }
+            set { SetPropertyValue<string>("pasifetiket", ref _pasifetiket, value); }
+        }
+
+        string _aktifetiket = "";
+        [Persistent("aktifetiket")]
+        [Size(1000)]
+        public string aktifetiket
+        {
+            get { return _aktifetiket; }
+            set { SetPropertyValue<string>("aktifetiket", ref _aktifetiket, value); }
+        }
+
+        string _serino = "";
+        [Persistent("serino")]
+        [Size(1000)]
+        public string serino
+        {
+            get { return _serino; }
+            set { SetPropertyValue<string>("serino", ref _serino, value); }
+        }
+    }
+}
